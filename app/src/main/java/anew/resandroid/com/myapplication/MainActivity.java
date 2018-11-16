f49d02d61058015c4fe5b2d1d058c108b0c5d669
RadBoris <rad.borislavov@gmail.com>
2018-11-15 18:54:45 -0600

    facebook login

--- app/src/main/java/anew/resandroid/com/myapplication/MainActivity.java
+++ app/src/main/java/anew/resandroid/com/myapplication/MainActivity.java
@@ -1,26 +1,40 @@
 package anew.resandroid.com.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.util.Log;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = this;
+
         Button button = this.loadLogInButton();
 
         Button smsButton = this.loadSMSButton();
-
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("check", "button has been clicked");
             }
+
         });
 
         smsButton.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +42,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("check", "sms button has been clicked");
             }
         });
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+
     }
 
     protected Button loadLogInButton(){
@@ -38,5 +74,16 @@ public class MainActivity extends AppCompatActivity {
     protected Button loadSMSButton(){
         Button button = findViewById(R.id.sms_button);
         return button;
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
